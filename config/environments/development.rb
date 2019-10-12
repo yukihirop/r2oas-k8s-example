c5bcad6013c020141c7fcaed7dd186a610c86699
yukihirop <te108186@gmail.com>
2019-10-12 20:21:15 +0900

    Configure rtsd

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  RoutesToSwaggerDocs.configure do |config|
+    config.namespace_type = :dot
+  end
 end
